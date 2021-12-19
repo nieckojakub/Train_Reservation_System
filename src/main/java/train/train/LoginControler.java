6d06003f78ebfd10e2afa309958afd3f771fb4fa
nieckojakub <niecko.jakub@gmail.com>
2021-12-19 17:52:47 +0100

    add Login image view(train,lock)

--- src/main/java/train/train/LoginControler.java
+++ src/main/java/train/train/LoginControler.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
@@ -18,10 +19,20 @@ public class LoginControler implements Initializable {
     private Button cancelButton;
     @FXML
     private Label loginMessageLabel;
+    @FXML
+    private ImageView trainImageView;
+    @FXML
+    private ImageView lockImageView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
         File brandingFile = new File("image/train.png");
+        Image trainImage = new Image(brandingFile.toURI().toString());
+        trainImageView.setImage(trainImage);
+
+        File lockFile = new File("image/lock.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
 
     }
 
